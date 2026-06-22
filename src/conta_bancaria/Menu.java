3d0e192c9cdd94d3e4412b0942a7b1de53f87447
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-06-21 23:37:13 -0300

    Menu Bancario

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,10 +1,80 @@
 package conta_bancaria;
 
+import java.util.Scanner;
+
 public class Menu {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner leia = new Scanner(System.in);
+		String opcao;
+		
+		System.out.println("                                                     ");
+		System.out.println("                    BANCO DO BRAZIL                  ");
+		System.out.println("                                                     ");
+		System.out.println("*****************************************************");
+		System.out.println("                                                     ");
+		System.out.println("            1 - Criar Conta                          ");
+		System.out.println("            2 - Listar todas as Contas               ");
+		System.out.println("            3 - Buscar Conta por Numero              ");
+		System.out.println("            4 - Atualizar Dados da Conta             ");
+		System.out.println("            5 - Apagar Conta                         ");
+		System.out.println("            6 - Sacar                                ");
+		System.out.println("            7 - Depositar                            ");
+		System.out.println("            8 - Transferir valores entre Contas      ");
+		System.out.println("            0 - Sair                                 ");
+		System.out.println("                                                     ");
+		System.out.println("*****************************************************");
+
+		while (true) {
+
+			System.out.print("Entre com a opção desejada: "); 
+			
+			opcao = leia.next().trim(); 
+
+			if (opcao.equals("0")) {
+				System.out.println("\nBanco do Brazil - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case "1":
+					System.out.println("Criar Conta\n\n");
+					break;
+				case "2":
+					System.out.println("Listar todas as Contas\n\n");
+					break;
+				case "3":
+					System.out.println("Consultar dados da Conta - por número\n\n");
+					break;
+				case "4":
+					System.out.println("Atualizar dados da Conta\n\n");
+					break;
+				case "5":
+					System.out.println("Apagar a Conta\n\n");
+					break;
+				case "6":
+					System.out.println("Saque\n\n");
+					break;
+				case "7":
+					System.out.println("Depósito\n\n");
+					break;
+				case "8":
+					System.out.println("Transferência entre Contas\n\n");
+					break;
+				default:
+					System.out.println("\nOpção Inválida!\n");
+					break;
+			}
+		}
 	}
-
+    
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Daniel Araujo");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conta_bancaria");
+		System.out.println("*********************************************************");
+	}
 }
